132eafde532eeaaa4f3fd31e7eb7fb59e0538f66
Eduardo Dourado <ed320819@gmail.com>
2025-10-18 11:35:14 +0000

    code cleanup and minor fixes

--- src/components/features/home/AboutSection.tsx
+++ src/components/features/home/AboutSection.tsx
@@ -10,8 +10,8 @@ export async function AboutSection() {
   const { base64 } = await getPlaiceholder(file);
 
   return (
-    <section className="bg-background py-6">
-      <div className="container mx-auto grid md:grid-cols-2 items-center px-2 sm:px-4 lg:px-6 gap-12">
+    <section className="bg-background">
+      <div className="container mx-auto grid md:grid-cols-2 items-center px-2 gap-12 sm:px-4 lg:px-6">
         <div className="relative h-180 rounded-lg overflow-hidden z-10">
             <Image 
               src="/about-image.jpg" 
@@ -24,14 +24,14 @@ export async function AboutSection() {
             />
         </div>
         <div 
-          className="relative z-9 sm:top-2 space-y-4 text-center p-8 rounded-lg md:-ml-16"
+          className="relative z-9 space-y-4 text-center p-12 md:p-23 rounded-lg sm:top-2 md:-ml-16"
           style={{ backgroundColor: '#1f304a' }}
         >
             <h3 className="text-lg md:text-xl text-gray-200">
               Antônio Gomes Lacerda
             </h3>
             <h1 className="text-4xl md:text-5xl font-bold text-white pt-6">&quot;A educação não tem preço, sua falta tem custo&quot;</h1>
-            <div className="pt-4">
+            <div className="pt-12 md:pt-20">
               <Button asChild variant="inverted" size="lg">
                 <Link href="/contato">INSCREVA-SE</Link>
               </Button>
